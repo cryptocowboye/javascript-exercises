7773a9316eec0f19f50c16e7bc822aeb490a2732
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-09-30 02:05:48 -0400

    Add code to repeat hey

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringHolder = string;
+    if (num > 0) {
+        for (i = 1; i < num; i++) {
+            string = string + stringHolder
+        }
+        return string
+    } else if (num === 0) {
+        return ''
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
