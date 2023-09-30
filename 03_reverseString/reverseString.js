aa1c085566c69d604b52433bc1e371e2369dce29
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-09-30 02:27:15 -0400

    Add code that reverses strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(letters) {
+    wordContainer = []
+    for (letter of letters) {
+        wordContainer.unshift(letter);
+    }
+    return wordContainer.join('')
 };
 
 // Do not edit below this line
