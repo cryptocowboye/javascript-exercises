f42ca4987a427dd8d48f92e120ef04f73b4e47bd
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-01 02:18:14 -0400

    Add code that sums all positive integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
 const sumAll = function() {
-
+    let sum = 0;
+    let argsList = [...arguments]
+    argsList.sort()
+    for (item of argsList) {
+        if (typeof item !== 'number') {
+            return 'ERROR'
+        }
+    }
+    for (i = argsList[0]; i <= argsList[argsList.length - 1]; i++) {
+        if (i < 0) {
+            return 'ERROR'
+        }
+        sum = sum + i
+    }
+    return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
