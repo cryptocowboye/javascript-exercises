405639d947fdb57612495275ff66a773b1644fcb
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-01 03:53:31 -0400

    Add code that converts temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let conversion = .5556 * (f - 32)
+  if (Number.isInteger(conversion)) {
+    return conversion
+  }
+  return parseFloat(conversion.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let conversion = (c * 1.8) + 32
+  if (Number.isInteger(conversion)) {
+    return conversion
+  }
+  return parseFloat(conversion.toFixed(1))
 };
 
 // Do not edit below this line
