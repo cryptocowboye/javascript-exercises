2a2fdcba009bff95f75d6425e3816c1209e012f2
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-01 00:47:46 -0400

    Add code that removes arguments from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, b = 0, c = 0, d = 0) {
+    const argsList = [...arguments]
+    argsList.shift()
+    let newList = []
+    for (i = 0; i < arr.length; i++) {
+        if (!argsList.includes(arr[i])) {
+            newList.push(arr[i])
+        }
+    }
+    return newList
 };
 
 // Do not edit below this line
