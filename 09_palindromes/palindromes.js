42063adcc5b8dfc41e5a6cbb44ea0e949202ac27
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-09 00:02:10 -0400

    Add code that checks for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let palindromeContainer = [];
+    let reverseContainer = [];
+    let numChecker = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    for (letter of str) {
+        if (letter.toLowerCase() !== letter.toUpperCase() || numChecker.includes(letter)) {
+            palindromeContainer.push(letter)
+            reverseContainer.unshift(letter)
+        }
+    }
+    let palindromeJoin = palindromeContainer.join('');
+    let reverseJoin = reverseContainer.join('')
+    if (palindromeJoin.toLowerCase() == reverseJoin.toLowerCase()) {
+        console.log('true')
+        return true
+    } else {
+        console.log('false')
+        return false
+    }
 };
 
 // Do not edit below this line
