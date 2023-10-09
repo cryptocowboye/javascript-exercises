9aa26ca2eb8e483e80d059d55fb45d5f34c20e9d
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-09 03:50:32 -0400

    Add code that finds the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let peopleByAge = people.sort((firstPerson, secondPerson) => {
+        let currentYear = 2023
+        if (firstPerson.yearOfDeath == undefined) {
+            firstPerson.yearOfDeath = currentYear
+        }
+        if (secondPerson.yearOfDeath == undefined) {
+            secondPerson.yearOfDeath = currentYear
+        }
+        firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth
+        secondPersonAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth
+        if (firstPersonAge < secondPersonAge) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    return peopleByAge[0]
 };
 
 // Do not edit below this line
