1cf30f243baff63627b495e27444f8261120298d
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2023-10-09 02:24:19 -0400

    Add code that picks fibonacci numbers by position

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num > 0) {
+        let newList = []
+        for (i = 0; i < num; i++) {
+            if (newList.length == 0 || newList.length == 1) {
+                newList.push(1)
+            } else {
+                newList.push(newList[newList.length - 1] + newList[newList.length - 2])
+            }
+        }
+        return newList[num - 1]
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
